cf6acf4019a091626a0c01118584da6fec402d2e
John Hill <john.m.hill91@gmail.com>
2019-03-25 16:15:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(num)
   if num <=1 
     false
-    elsif n ==2 
+    elsif num ==2 
     true
-  else (2..n/2).none? { |i| n%i ==0}
+  else (2..num/2).none? { |i| num%i ==0}
   end
 end 
